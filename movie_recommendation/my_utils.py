1742448eb4873557997e854901b6ab3f6e98e07b
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2019-12-23 17:35:59 +0530

    Create my_utils.py

---
+++ movie_recommendation/my_utils.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 23 14:52:00 2019
+
+@author: Achyutha.aluru
+"""
+import pandas as pd
+import pickle
+from keras.models import load_model
+
+model = load_model('final_train1.h5')
+
+df = pd.read_csv('df.csv')
+df['tag']=df['tag'].fillna("")
+pickle_in = open("genres_map.pickle","rb")
+genres_map = pickle.load(pickle_in)
+
+pickle_in = open("tag_map.pickle","rb")
+tag_map = pickle.load(pickle_in)
+
+def result(movie_copy,most_similar):
+
+    recommendations_list = []
+    watched_list = [] 
+
+    for i in movie_copy:
+        watched_list.append(df.loc[df['movieId'] == i].values[0])
+    watched_movies = pd.DataFrame(watched_list, columns=['movieId','Title','Genres','Tags'])
+        
+    for i in most_similar:
+        if i not in movie_copy:
+            recommendations_list.append(df.loc[df['movieId'] == i].values[0])
+    rec_movies = pd.DataFrame(recommendations_list, columns=['movieId','Title','Genres','Tags'])
+    
+    return watched_movies,rec_movies
