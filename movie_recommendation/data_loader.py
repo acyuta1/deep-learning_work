2b60e3233549a7873da4a4b2cf9fbf34f6d64b46
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2019-12-24 18:03:40 +0530

    Update data_loader.py

--- movie_recommendation/data_loader.py
+++ movie_recommendation/data_loader.py
@@ -11,25 +11,28 @@ from keras.models import load_model
 model = load_model('final_train1.h5')
 
 df = pd.read_csv('df.csv')
-df['tag']=df['tag'].fillna("")
-pickle_in = open("genres_map.pickle","rb")
+df['tag'] = df['tag'].fillna("")
+pickle_in = open("genres_map.pickle", "rb")
 genres_map = pickle.load(pickle_in)
 
-pickle_in = open("tag_map.pickle","rb")
+pickle_in = open("tag_map.pickle", "rb")
 tag_map = pickle.load(pickle_in)
 
-def result(movie_copy,most_similar):
+
+def result(movie_copy, most_similar):
 
     recommendations_list = []
-    watched_list = [] 
+    watched_list = []
 
     for i in movie_copy:
         watched_list.append(df.loc[df['movieId'] == i].values[0])
-    watched_movies = pd.DataFrame(watched_list, columns=['movieId','Title','Genres','Tags'])
-        
+    watched_movies = pd.DataFrame(
+        watched_list, columns=['movieId', 'Title', 'Genres', 'Tags'])
+
     for i in most_similar:
         if i not in movie_copy:
             recommendations_list.append(df.loc[df['movieId'] == i].values[0])
-    rec_movies = pd.DataFrame(recommendations_list, columns=['movieId','Title','Genres','Tags'])
-    
-    return watched_movies,rec_movies
+    rec_movies = pd.DataFrame(recommendations_list, columns=[
+                              'movieId', 'Title', 'Genres', 'Tags'])
+
+    return watched_movies, rec_movies
