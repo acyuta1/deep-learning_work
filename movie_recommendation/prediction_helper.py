e4927bf9a500d13700ade70c1fa99abb9b2fcdd4
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-01 00:41:45 +0530

    Update prediction_helper.py

--- movie_recommendation/prediction_helper.py
+++ movie_recommendation/prediction_helper.py
@@ -1,54 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 23 14:56:03 2019
+
+@author: Achyutha.aluru
+"""
+
 import numpy as np
-from data_loader import DF, GENRES_MAP, TAG_MAP
+from data_loader import DF, GENRES_MAP, TAG_MAP, pad, INPUT_LENGTH
+
 
 def make_prediction(movies, g_input, t_input, model):
-    '''
+    """
     Function to predict provided we have the processed inputs. 
-    '''
+    """
     return model.predict(
-            [np.array([movies, ]), np.array([g_input, ]), np.array([t_input, ])])
+        [np.array([movies,]), np.array([g_input,]), np.array([t_input,])]
+    )
 
 
 def prepare_inputs(movie_input):
-    '''
+    """
     With only MovieID's as the input to our model, we are required to 
     obtain the required genre's and tag's and bring our raw input to
     the required shape.
-    '''
-    movies_copy = movie_input
+    """
+    movies_copy = movie_input[:]
 
-    genres, tags, genre_input, tag_input = [], [], [], []
-    
-    for i in movie_input:
-        genres.append(DF.loc[DF['movieId'] == i, 'genres'].iloc[0])
-        tags.append(DF.loc[DF['movieId'] == i, 'tag'].iloc[0])
+    genres = (DF.genres[DF["movieId"].isin(movie_input)].values).tolist()
+    genres = [word for line in genres for word in line.split()]
 
-    for i in genres:
-        for j in i.split():
-            genre_input.append(list(GENRES_MAP.keys())[
-                                   list(GENRES_MAP.values()).index(j)])
-    
-    genre_input = list(set(genre_input))
-    required_length = 24 - len(genre_input)
-    genre_input = np.pad(genre_input, (0, required_length), 'constant')
+    tags = (DF.tag[DF["movieId"].isin(movie_input)].values).tolist()
+    tags = [word for line in tags for word in line.split()]
 
-    for i in tags:
-        for j in i.split():
-            if j in ('em','cs','se'):
-                continue
-            else:
-                tag_input.append(list(TAG_MAP.keys())[
-                                 list(TAG_MAP.values()).index(j)])
+    genres = [
+        list(GENRES_MAP.keys())[list(GENRES_MAP.values()).index(i)] for i in genres
+    ]
+    genres = list(set(genres))
+    genres = pad(genres, INPUT_LENGTH[1])
     
-    tag_input = list(set(tag_input))
-    
-    if len(tag_input) > 100:
-        tag_input = tag_input[0:100]
+    tags = [
+        list(TAG_MAP.keys())[list(TAG_MAP.values()).index(i)]
+        for i in tags
+        if i not in ("em", "cs", "se")
+    ]
+    tags = list(set(tags))
+    if len(tags) > 100:
+        tags = tags[0:100]
     else:
-        required_length = 100 - len(tag_input)
-        tag_input = np.pad(tag_input, (0, required_length), 'constant')
-
-    required_length = 2698 - len(movie_input)
-    movie_input = np.pad(movie_input, (0, required_length), 'constant')
-
-    return movie_input, genre_input, tag_input, movies_copy
+        tags = pad(tags, INPUT_LENGTH[2])
+        
+    movie_input = pad(movie_input, INPUT_LENGTH[0])
+    
+    return movie_input, genres, tags, movies_copy
