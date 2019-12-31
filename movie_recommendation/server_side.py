1639a495f76b0563d26ad357da3b6700b695bc4e
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-01 00:42:30 +0530

    Update server_side.py

--- movie_recommendation/server_side.py
+++ movie_recommendation/server_side.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# -*- coding: utf-8 -*-
-
-
 from data_loader import MODEL, result
 from prediction_helper import prepare_inputs, make_prediction
 from flask import Flask, render_template, request
@@ -10,40 +5,40 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        comment = request.form['comment']
-        movies = [int(x) for x in comment.split(',')]
-        movies = list(set(movies))
+        comment = request.form["comment"]
+        movies = [int(x) for x in comment.split(",")]
 
         movies, g_input, t_input, movie_copy = prepare_inputs(movies)
 
-        print(movies, t_input, g_input)
-        
+        print(movies[:10],g_input,t_input)
+
         preds = make_prediction(movies, g_input, t_input, MODEL)
 
-        most_similar = preds[0].argsort()[-(10+len(movie_copy)):][::-1]
+        most_similar = preds[0].argsort()[-(10 + len(movie_copy)) :][::-1]
 
         watched_movies, rec_movies = result(movie_copy, most_similar)
 
-        context = {"table1": [watched_movies.to_html(classes='data')],
-                   "title1": watched_movies.columns.values,
-                   "table2": [rec_movies.to_html(classes='data')],
-                   "title2": rec_movies.columns.values
-                   }
+        context = {
+            "table1": [watched_movies.to_html(classes="data")],
+            "title1": watched_movies.columns.values,
+            "table2": [rec_movies.to_html(classes="data")],
+            "title2": rec_movies.columns.values,
+        }
 
-    return render_template('prediction.html', context=context)
+    return render_template("prediction.html", context=context)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     global MODEL
     MODEL = MODEL
     app.run(threaded=False)
