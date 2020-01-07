21b25973eea842f48678a5160d6a571af8ad1c4a
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-07 16:01:49 +0530

    Update and rename server_side.py to server.py

---
+++ movie_recommendation/server.py
@@ -0,0 +1,41 @@
+from prediction_helper import prepare_inputs, make_prediction
+from data_loader import MODEL, result, MOVIE_NAMES
+from flask import Flask, render_template, request
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def home():
+    return render_template("home.html", data = MOVIE_NAMES)
+
+
+@application.route("/predict", methods=["POST"])
+def predict():
+
+    if request.method == "POST":
+
+        movies = request.form.getlist('mymultiselect')        
+        movies, g_input, movie_copy = prepare_inputs(movies)
+
+        preds = make_prediction(movies, g_input, MODEL)
+
+        most_similar = preds[0].argsort()[-(10 + len(movie_copy)) :][::-1]
+
+        watched_movies, rec_movies = result(movie_copy, most_similar)
+
+        context = {
+            "watched_movies_table": [watched_movies.to_html(classes="data")],
+            "watched_movies_titles": watched_movies.columns.values,
+            "rec_movies_table": [rec_movies.to_html(classes="data")],
+            "rec_movies_titles": rec_movies.columns.values,
+        }
+
+    return render_template("prediction.html", context=context)
+
+if __name__ == "__main__":
+    global MODEL
+    MODEL = MODEL
+    global MOVIE_NAMES
+    MOVIE_NAMES = MOVIE_NAMES
+    application.run(threaded=False)
