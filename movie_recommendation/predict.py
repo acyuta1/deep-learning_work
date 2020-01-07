487757ebb3fc73d60f28f30b2f9934dffd040a6f
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-07 16:01:09 +0530

    Update and rename prediction_helper.py to predict.py

--- movie_recommendation/predict.py
+++ movie_recommendation/predict.py
@@ -2,12 +2,12 @@ import numpy as np
 from data_loader import DF, GENRES_MAP, TAG_MAP, pad, INPUT_LENGTH
 
 
-def make_prediction(movies, g_input, t_input, model):
+def make_prediction(movies, g_input, model):
     """
     Function to predict provided we have the processed inputs. 
     """
     return model.predict(
-        [np.array([movies,]), np.array([g_input,]), np.array([t_input,])]
+        [np.array([movies,]), np.array([g_input,])]
     )
 
 
@@ -17,31 +17,18 @@ def prepare_inputs(movie_input):
     obtain the required genre's and tag's and bring our raw input to
     the required shape.
     """
+    
+    movie_input = [DF.movieId[DF['title']==i].iloc[0] for i in movie_input]
     movies_copy = movie_input[:]
 
     genres = (DF.genres[DF["movieId"].isin(movie_input)].values).tolist()
     genres = [word for line in genres for word in line.split()]
 
-    tags = (DF.tag[DF["movieId"].isin(movie_input)].values).tolist()
-    tags = [word for line in tags for word in line.split()]
-
     genres = [
         list(GENRES_MAP.keys())[list(GENRES_MAP.values()).index(i)] for i in genres
     ]
-    genres = list(set(genres))
-    genres = pad(genres, INPUT_LENGTH["genre_len"])
     
-    tags = [
-        list(TAG_MAP.keys())[list(TAG_MAP.values()).index(i)]
-        for i in tags
-        if i not in ("em", "cs", "se")
-    ]
-    tags = list(set(tags))
-    if len(tags) > 100:
-        tags = tags[0:100]
-    else:
-        tags = pad(tags, INPUT_LENGTH["tag_len"])
-        
+    genres = pad(genres, INPUT_LENGTH["genre_len"])    
     movie_input = pad(movie_input, INPUT_LENGTH["movie_len"])
     
-    return movie_input, genres, tags, movies_copy
+    return movie_input, genres, movies_copy
