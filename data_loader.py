c5cd2629ce605ed20f46bb310696c632feb3b42a
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-13 02:33:08 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,18 +1,12 @@
-import pickle
 import pandas as pd
 from keras.models import load_model
 
-MODEL = load_model("model.h5")
+MODEL = load_model("victory1.h5")
 
-DF = pd.read_csv("new_df.csv")
+DF = pd.read_csv("df_final.csv")
 DF["tag"] = DF["tag"].fillna("")
-PICKLE_IN = open("genres_map.pickle", "rb")
-GENRES_MAP = pickle.load(PICKLE_IN)
 
-PICKLE_IN = open("tag_map.pickle", "rb")
-TAG_MAP = pickle.load(PICKLE_IN)
-
-INPUT_LENGTH = {"movie_len":36, "genre_len":129}
+INPUT_LENGTH = {"movie_len":36, "genre_len":36}
 MOVIE_NAMES = DF.title.values.tolist()
 
 def pad(lst, width):
@@ -25,17 +19,15 @@ def pad(lst, width):
 
 def result(movie_copy, most_similar):
     """
-    From the predicted indices (MovieId's), the corresponding movies are extracted 
+    From the prediction indices, the corresponding movies are extracted 
     from the dataset.
-    Also the original "watched" movie rows are extracted for comparison purposes.
+    Also the original "watched" movie information is obtained.
     We return two dataframes: Watched and the Recommendations.
     """
 
-    rec_movies = DF.set_index("movieId").loc[most_similar].reset_index()    
-    blankIndex=[''] * len(rec_movies)
-    rec_movies.index=blankIndex
-
+    rec_movies = DF.set_index("movieId").loc[most_similar].reset_index()
+    rec_movies = rec_movies.iloc[:,:-1]
+    rec_movies = rec_movies[~rec_movies["movieId"].isin(movie_copy)]
     watched_movies = DF.set_index("movieId").loc[movie_copy].reset_index()
-    blankIndex=[''] * len(watched_movies)
-    watched_movies.index=blankIndex
+    watched_movies = watched_movies.iloc[:,1:]
     return watched_movies, rec_movies
