059d0ef1d33175504890adb3c3b576e956aac679
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-15 02:29:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from predict import prepare_inputs, make_prediction
-from data_loader import MODEL, result, MOVIE_NAMES
+from prediction_helper import prepare_inputs, make_prediction
 from flask import Flask, render_template, request
+from data_loader import MODEL1, MODEL2, result, MOVIE_NAMES
 
 application = Flask(__name__)
 
@@ -15,27 +15,33 @@ def predict():
 
     if request.method == "POST":
 
-        movies = request.form.getlist('mymultiselect')        
-        movies, g_input, movie_copy = prepare_inputs(movies)
-
-        preds = make_prediction(movies, g_input, MODEL)
-
-        most_similar = preds[0].argsort()[-(10 + len(movie_copy)) :][::-1]
-
-        watched_movies, rec_movies = result(movie_copy, most_similar)
+        movies = request.form.getlist('mymultiselect')
+        
+        if(len(movies)<7):
+            g_input, movie_input, movie_copy = prepare_inputs(movies,1)
+            preds = make_prediction(g_input, MODEL2, movie_input)
+            watched_movies, rec_movies = result(movie_copy, preds)
+            
+        else:
+            movies, g_input, movie_copy = prepare_inputs(movies,2)
+            print(movies[:10],g_input[:10])
+            preds = make_prediction(movies, g_input, MODEL1, movie_copy)
+            most_similar = preds[0].argsort()[-(10 + len(movie_copy)) :][::-1]
+            watched_movies, rec_movies = result(movie_copy, most_similar)
 
         context = {
-            "watched_movies_table": [watched_movies.to_html(classes="data")],
-            "watched_movies_titles": watched_movies.columns.values,
-            "rec_movies_table": [rec_movies.to_html(classes="data")],
-            "rec_movies_titles": rec_movies.columns.values,
+            "table1": [watched_movies.to_html(classes="data")],
+            "title1": watched_movies.columns.values,
+            "table2": [rec_movies.to_html(classes="data")],
+            "title2": rec_movies.columns.values,
         }
 
     return render_template("prediction.html", context=context)
 
+
 if __name__ == "__main__":
     global MODEL
-    MODEL = MODEL
-    global MOVIE_NAMES
+    MODEL1 = MODEL1
+    MODEL2 = MODEL2
     MOVIE_NAMES = MOVIE_NAMES
     application.run(threaded=False)
