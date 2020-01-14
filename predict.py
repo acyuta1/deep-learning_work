d0899613707af04bacb6e0f0715ba4194e14e60a
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-15 02:28:56 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,29 +1,65 @@
 import numpy as np
-from data_loader import DF, pad, INPUT_LENGTH
+from random import shuffle
+from data_loader import DF, pad, INPUT_LENGTH, GENRES_TOKENS
 
+    
 
-def make_prediction(movies, g_input, model):
+def make_prediction(*arg):
     """
     Function to predict provided we have the processed inputs. 
     """
-    return model.predict(
-        [np.array([movies,]), np.array([g_input,])]
-    )
+    if len(arg)==3:
+        movie_pred_indices = []
+        g_input = arg[0] 
+        model = arg[1]
+        movie_copy = arg[2]
+        print(movie_copy, g_input)
+        
+        for i in movie_copy:
+            preds = model.predict([([g_input[i],])])
+            most_similar = preds[0].argsort()[-30:][::-1]
+            movie_pred_indices.append(most_similar.tolist())    
+        movie_pred_indices = [item for sublist in movie_pred_indices 
+                              for item in sublist]
+        movie_pred_indices = list(set(movie_pred_indices))
+        shuffle(movie_pred_indices)
+        movie_pred_indices = [i+1 for i in movie_pred_indices]
+        return np.array(movie_pred_indices[:15])
+        
+    else:
+        movies = arg[0] 
+        g_input = arg[1]
+        model = arg[2]
+        
+        return model.predict(
+        [np.array([movies,]), np.array([g_input,])])
 
 
-def prepare_inputs(movie_input):
+
+def prepare_inputs(*args):
     """
     With only MovieID's as the input to our model, we are required to 
     obtain the required genre's and tag's and bring our raw input to
     the required shape.
     """
+    if args[1]==1:        
+        movie_input = args[0]
+        movie_input = [DF.movieId[DF['title']==i].iloc[0] for i in movie_input]
+        movies_copy = movie_input[:]
+        movie_input = [i-1 for i in movie_input]
+        g_input = GENRES_TOKENS
+        return g_input, movie_input, movies_copy
+    else:
+        movie_input = args[0]
+        movie_input = [DF.movieId[DF['title']==i].iloc[0] for i in movie_input]
+        movies_copy = movie_input[:]
     
-    movie_input = [DF.movieId[DF['title']==i].iloc[0] for i in movie_input]
-    movies_copy = movie_input[:]
-
-    genres = (DF.genres_class[DF["movieId"].isin(movie_input)].values).tolist()
-
-    genres = pad(genres, INPUT_LENGTH["genre_len"])    
-    movie_input = pad(movie_input, INPUT_LENGTH["movie_len"])
+        genres = (DF.genres_class[DF["movieId"].isin(movie_input)].values).tolist()
     
-    return movie_input, genres, movies_copy
+        genres = pad(genres, INPUT_LENGTH["genre_len"])    
+        movie_input = pad(movie_input, INPUT_LENGTH["movie_len"])
+        movie_input = np.asarray(movie_input).astype('float32')/9724
+        
+        return movie_input, genres, movies_copy
+        
+
