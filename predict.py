609ff5012b866ca20f104e022c31a98857120705
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-16 01:20:49 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,8 +1,6 @@
 import numpy as np
 from random import shuffle
-from data_loader import DF, pad, INPUT_LENGTH, GENRES_TOKENS
-
-    
+from data_loader import DF, pad, INPUT_LENGTH, GENRES_TOKENS, sample
 
 def make_prediction(*arg):
     """
@@ -13,8 +11,7 @@ def make_prediction(*arg):
         g_input = arg[0] 
         model = arg[1]
         movie_copy = arg[2]
-        print(movie_copy, g_input)
-        
+        print(movie_copy, g_input)        
         for i in movie_copy:
             preds = model.predict([([g_input[i],])])
             most_similar = preds[0].argsort()[-30:][::-1]
@@ -24,17 +21,22 @@ def make_prediction(*arg):
         movie_pred_indices = list(set(movie_pred_indices))
         shuffle(movie_pred_indices)
         movie_pred_indices = [i+1 for i in movie_pred_indices]
-        return np.array(movie_pred_indices[:15])
-        
+        return np.array(movie_pred_indices[:15])           
     else:
-        movies = arg[0] 
+        movies = arg[0]
         g_input = arg[1]
         model = arg[2]
-        
-        return model.predict(
-        [np.array([movies,]), np.array([g_input,])])
-
-
+        next_movie = []
+        for i in range(40): #generate 20 movies
+            array_seed = np.asarray(movies).astype('float32')/9724
+            preds = model.predict([np.array([array_seed,]),np.array([g_input,])])[0]
+            next_index = sample(preds)
+            next_movie.append(next_index[0])
+            movies.append(next_index[0])
+            g_input.append(next_index[1])       
+            movies = movies[1:]
+            g_input = g_input[1:]    
+        return next_movie
 
 def prepare_inputs(*args):
     """
@@ -53,13 +55,9 @@ def prepare_inputs(*args):
         movie_input = args[0]
         movie_input = [DF.movieId[DF['title']==i].iloc[0] for i in movie_input]
         movies_copy = movie_input[:]
-    
         genres = (DF.genres_class[DF["movieId"].isin(movie_input)].values).tolist()
-    
         genres = pad(genres, INPUT_LENGTH["genre_len"])    
         movie_input = pad(movie_input, INPUT_LENGTH["movie_len"])
-        movie_input = np.asarray(movie_input).astype('float32')/9724
+        #movie_input = np.asarray(movie_input).astype('float32')
         
         return movie_input, genres, movies_copy
-        
-
