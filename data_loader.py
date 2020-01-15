cd7392575101df9691f34fbd27c2736a02a2acd0
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-16 01:20:17 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,19 +2,32 @@ from keras import backend as K
 import pandas as pd
 from keras.models import load_model
 from numpy import loadtxt
-
+import numpy as np
+import pickle
 
 DF = pd.read_csv("df_final.csv")
 DF["tag"] = DF["tag"].fillna("")
 GENRES_TOKENS = loadtxt('genres_tokens.csv', delimiter=',') 
-INPUT_LENGTH = {"movie_len":36, "genre_len":36}
+INPUT_LENGTH = {"movie_len":18, "genre_len":18}
 MOVIE_NAMES = DF.title.values.tolist()
 
-def full_multi_label_metric(y_true, y_pred):
-    comp = K.equal(y_true, K.round(y_pred))
-    return K.cast(K.all(comp, axis=-1), K.floatx())
+with open('genre_movie_dict.pickle', 'rb') as fp:
+    GENRE_MOVIE_DICT = pickle.load(fp)
 
-MODEL1 = load_model("done.h5",custom_objects={"full_multi_label_metric": full_multi_label_metric})
+def sample(preds, temperature=0.1):
+    '''
+    Reweighting the distribution to a certain "temperature".
+    Sampling the next movie at random according to the reweighted distribution
+    Adding the new movie at the end of the available sequence prediction.
+    '''
+    preds = np.asarray(preds).astype('float64')
+    exp_preds = preds - np.exp(temperature)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    argmax = np.argmax(probas)
+    return [argmax,GENRE_MOVIE_DICT[argmax]]
+
+MODEL1 = load_model("generated_seed_without_opt.h5")
 MODEL2 = load_model("single_withoutopt.h5")
 
 def pad(lst, width):
