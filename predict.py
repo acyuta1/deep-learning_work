e3feead81a5463b38821565411495d1f6bb7b9e5
Achyutha Mithra Aluru <56015756+achyutha-mithra@users.noreply.github.com>
2020-01-18 01:22:01 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ def make_prediction(*arg):
     """
     Function to predict provided we have the processed inputs. 
     """
-    if len(arg)==3:
+    if arg[3]==True:
         movie_pred_indices = []
         g_input = arg[0] 
         model = arg[1]
@@ -32,6 +32,16 @@ def make_prediction(*arg):
         movies = [DF.movieId[DF.movieId.isin(MOVIES_LIST[i])].values.tolist() for 
                              i in top_3_sequence]
         movies = [item for sublist in movies for item in sublist]
+#        next_movie = []
+#        for i in range(40): #generate 20 movies
+#            array_seed = np.asarray(movies).astype('float32')/9724
+#            preds = model.predict([np.array([array_seed,]),np.array([g_input,])])[0]
+#            next_index = sample(preds)
+#            next_movie.append(next_index[0])
+#            movies.append(next_index[0])
+#            g_input.append(next_index[1])       
+#            movies = movies[1:]
+#            g_input = g_input[1:]    
         return movies
         
 
@@ -41,7 +51,7 @@ def prepare_inputs(*args):
     obtain the required genre's and tag's and bring our raw input to
     the required shape.
     """
-    if args[1]==1:        
+    if args[1]==True:        
         movie_input = args[0]
         movie_input = [DF.movieId[DF['title']==i].iloc[0] for i in movie_input]
         movies_copy = movie_input[:]
